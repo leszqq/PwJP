9fa4bb1693bbd1f0f13759c69ab74304eb4c01e0
wiktor <comradewiktor@gmail.com>
2020-10-29 11:10:12 +0100

    zadanie 11: iloczyn skalarny

---
+++ iloczyn_skalarny_z11.py
@@ -0,0 +1,15 @@
+"""
+Autor: Wiktor Lechowicz
+Ten skrypt sluzy do obliczania wartosci iloczynu skalarnego 2 wektorow
+"""
+
+a = [1, 2, 12, 4]
+b = [2, 4, 2, 8]
+
+def product(a, b):
+    p = 0
+    for i in range(len(a)):
+        p += a[i]*b[i]
+    return p
+
+print(product(a, b))
